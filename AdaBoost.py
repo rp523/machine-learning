05fe4820a09d6e81ff6189bb7b6e4abc72e6ae96
Yusuke Goto <me@example.com>
2017-06-06 23:27:22 +0900

    -m

--- AdaBoost.py
+++ AdaBoost.py
@@ -1,22 +1,34 @@
 import numpy as np
-from hog import CHogParam,CHog
-import imgtool as imt
-import mathtool as mt
+#from feature.hog import CHogParam,CHog
+from feature.hog import *
+from feature.chnFtrs import *
+import common.imgtool as imt
+import common.mathtool as mt
 import pandas as pd
-import fileIO as fio
+import common.fileIO as fio
 import os
 import scipy.io as sio
 from matplotlib import pyplot as plt
 from PIL import Image
 import gui
 from enum import Enum
-from conda._vendor.toolz.itertoolz import accumulate
+from common.origLib import *
+from gaussianProcess import *
+from input import *
+from preproc import *
+from decisionTree import *
 
-class adaType(Enum):
-    Discrete = 1
-    Real = 2
-    SaturatedReal = 3
 
+class AdaBoostParam(CParam):
+    def __init__(self):
+        setDicts = dicts()
+        setDicts["Type"] = selparam("Discrete", "Real", "SaturatedReal")
+        setDicts["Bin"] = 32
+        setDicts["Loop"] = 10000
+        setDicts["Regularizer"] = 1e-5
+        super().__init__(setDicts) 
+
+'''
 # for文を使わずに投票されたBINの数を数えるためのクラス
 class VoteCount:
     def __init__(self,binNum,detectorNum,sampleNum):
@@ -29,20 +41,20 @@ class VoteCount:
     # 弱識別器の次元を１つ減らす
     def reduceDetector(self):
         self.__filter = np.delete(self.__filter, 0, axis=1) # 減らす列の位置はどこでもいい
-
+'''
+        
 class CAdaBoost:
     
-    def __init__(self,inAdaType,inAdaBin,inImgList,inLabelList,inDetectorList,loopNum):
-        self.__adaType = inAdaType
-        if self.__adaType == adaType.Discrete:
-            self.__bin = 2
-        else:
-            self.__bin = inAdaBin
-        self.__loopNum = loopNum
+    def __init__(self,inAdaBoostParam,inImgList,inLabelList,inDetectorList):
+        self.__adaType = inAdaBoostParam["Type"].nowSelected()
+        self.__bin = inAdaBoostParam["Bin"]
+        self.__loopNum = inAdaBoostParam["Loop"]
+        self.__regularize = inAdaBoostParam["Regularizer"]
         self.__detectorList = inDetectorList
         self.__imgList = inImgList
         self.__labelList = np.array(inLabelList)
         self.__featureLen = None
+        
         self.__thresh = np.zeros(len(inDetectorList),float)
         
         self.__Prepare()
@@ -52,7 +64,6 @@ class CAdaBoost:
     # (弱識別器インデックス) x (入力イメージインデックス) の行列を計算する
     # AdaBoostのループにおいて、この行列は最初に一度だけ計算する
     def __Prepare(self):
-        
         if not os.path.exists("Train.mat"):
             self.__trainScoreMat = np.empty((0,self.__GetFeatureLength()),float)
     
@@ -80,7 +91,7 @@ class CAdaBoost:
         if None == self.__featureLen:
             self.__featureLen = 0
             for detector in self.__detectorList:
-                self.__featureLen = self.__featureLen + detector.GetFeatureLength()
+                self.__featureLen = self.__featureLen + detector.GetFeatureLength2()
         return self.__featureLen
 
     # PosとNegが混在しているスコア行列からPosのみ、Negのみのスコア行列に分離する
@@ -108,19 +119,26 @@ class CAdaBoost:
             return self.__reliability[bin]
 
     def __Boost(self):
-        
+
         if os.path.exists("strong.mat"):
+            print("ARU")
             return
         
+        assert(isinstance(self.__trainScoreMat, np.ndarray))
+        assert(self.__trainScoreMat.ndim == 2)
+        assert(not np.any(self.__trainScoreMat < 0))
         # ブースティングでは識別器ごとの性能を比較するので、
         # 識別器IDが浅い階層に来るよう転置をとる
         self.__trainScoreMat = np.transpose(self.__trainScoreMat)
 
         detectorNum = self.__trainScoreMat.shape[0]
+        sampleNum   = self.__trainScoreMat.shape[1]
         detLen = min(self.__loopNum,detectorNum)
  
         # スコア行列をPos/Negで分ける
         trainPosScore, trainNegScore = self.__PosNegDevide(self.__trainScoreMat, self.__labelList)
+        assert(not np.any(trainPosScore < 0))
+        assert(not np.any(trainNegScore < 0))
         posSample = trainPosScore.shape[1]
         negSample = trainNegScore.shape[1]
 
@@ -133,73 +151,46 @@ class CAdaBoost:
         # 強識別器情報の記録メモリを確保
         strongDetBin = np.zeros((detLen,self.__bin),float)
         strongDetID = np.zeros(detLen,int)
-        parity = np.zeros(detLen,int)
 
-        if self.__adaType == adaType.Discrete:
+        if self.__adaType == "Discrete":
+            
+            self.__decisionTree = DecisionTree(scoreMat = self.__trainScoreMat,
+                                        labelVec = self.__labelList,
+                                        maxDepth = 2)
+            scoreMat = self.__decisionTree.predict(np.arange(detectorNum), self.__trainScoreMat)
+            detIdxSaved = np.arange(detectorNum)
+            detIdx = detIdxSaved
+            self.__detWeights = np.empty(detLen)
 
+            sampleWeights = np.ones(sampleNum)
+            sampleWeights[self.__labelList > 0] /= np.sum(self.__labelList > 0)
+            sampleWeights[self.__labelList < 0] /= np.sum(self.__labelList < 0)
+
+            yfMatSaved = (scoreMat * self.__labelList).astype(np.int)
+            yfMat = yfMatSaved
+            errorMatSaved = (yfMat < 0).astype(np.int)
+            errorMat = errorMatSaved
+            
             for w in range(detLen):
-
-                errorRateVec = np.zeros(detLen,float)
-                threshVec = np.zeros(detLen,float)
-                parityVec = np.zeros(detLen,float)
-
-                for weakCandidate in range(detLen):
-                    scoreColumn = np.transpose(np.array([list(self.__trainScoreMat[weakCandidate])]))
-                    oneRow = np.ones((1,posSample+negSample),float)
-                    scoreCopyMat = np.dot(scoreColumn, oneRow)
-    
-                    print(w,weakCandidate,self.__trainScoreMat[weakCandidate].shape,scoreCopyMat.shape)
-                    errorRateP = np.sum(   \
-                        (self.__trainPosScore[weakCandidate] < scoreCopyMat) * (self.__labelList ==  1) +    \
-                        (self.__trainPosScore[weakCandidate] > scoreCopyMat) * (self.__labelList == -1)      \
-                        ,axis=1)
-                    errorRatePmin = np.min(errorRateP)
-                    errorRateN = np.sum(   \
-                        (self.__trainNegScore[weakCandidate] > scoreCopyMat) * (self.__labelList ==  1) +    \
-                        (self.__trainNegScore[weakCandidate] < scoreCopyMat) * (self.__labelList == -1)      \
-                        ,axis=1)
-                    errorRateNmin = np.min(errorRateN)
-                    
-                    # for文を抜けても残る情報
-                    if (errorRatePmin < errorRateNmin):
-                        errorRateVec[weakCandidate] = errorRatePmin
-                        threshVec[weakCandidate] = self.__trainPosScore[weakCandidate][np.argmin(errorRatePmin)]
-                        parityVec[weakCandidate] = 1
-                    else:
-                        errorRateVec[weakCandidate] = errorRateNmin
-                        threshVec[weakCandidate] = self.__trainNegScore[weakCandidate][np.argmin(errorRateNmin)]
-                        parityVec[weakCandidate] = -1
                 
-                # 選択した最優秀識別器のスコアをもとに、サンプル重みを更新
-                bestDet = np.argmin(errorRateVec)
-                self.__thresh[w] = threshVec[bestDet]
-                errorRate = errorRateVec[bestDet]
+                assert(detIdx.size == yfMat.shape[0] == errorMat.shape[0])
+                errorRates = np.sum(errorMat * sampleWeights, axis = 1)
+                bestIdx = np.argmin(errorRates)
+                strongDetID[w] = detIdx[bestIdx]
+                epsilon = 1e-10
+                reliability = 0.5 * np.log((1.0 - errorRates[bestIdx] + epsilon) / (errorRates[bestIdx] + epsilon))
+                self.__detWeights[w] = reliability
+                sampleWeights = sampleWeights * np.exp(- reliability * yfMat[bestIdx])
+                sampleWeights[self.__labelList > 0] /= np.sum(sampleWeights[self.__labelList > 0])
+                sampleWeights[self.__labelList < 0] /= np.sum(sampleWeights[self.__labelList < 0])
+                
+                detIdx = np.delete(detIdx, bestIdx)
+                yfMat  = np.delete(yfMat,  bestIdx, axis = 0)
+                errorMat = np.delete(errorMat, bestIdx, axis = 0)
 
-                epsilon = 0.00001
-                reliability = 0.5 * np.log((1.0 - errorRate + epsilon) / (errorRate + epsilon))
-                strongDetBin[w] = np.array([- parityVec[bestDet] * reliability, parityVec[bestDet] * reliability])
-                strongDetID[w] = bestDet
-    
-                # サンプル重みを更新&正規化する
-                posExponent = -1.0 * (strongDetBin[w][0] * (trainPosScore[bestDet] <= self.__thresh[w]) + \
-                                      strongDetBin[w][1] * (trainPosScore[bestDet] >  self.__thresh[w]))
-                posMax = np.max(posExponent)
-                posSampleWeight *= np.exp(posExponent - posMax)
-                posSampleWeight /= np.sum(posSampleWeight)
-
-                negExponent =  1.0 * (strongDetBin[w][0] * (trainNegScore[bestDet] <= self.__thresh[w]) + \
-                                      strongDetBin[w][1] * (trainNegScore[bestDet] >  self.__thresh[w]))
-                negMax = np.max(negExponent)
-                negSampleWeight *= np.exp(negExponent - negMax)
-                negSampleWeight /= np.sum(negSampleWeight)
-
-                # 選択除去された弱識別器の情報を次ループでは考えない
-                trainPosScore = np.delete(trainPosScore, bestDet, axis=0)
-                trainNegScore = np.delete(trainNegScore, bestDet, axis=0)
-                posSample -= 1
-                negSample -= 1
-
-        elif (self.__adaType == adaType.Real) or (self.__adaType == adaType.SaturatedReal):
+        elif (self.__adaType == "Real") or (self.__adaType == "SaturatedReal"):
+            
+            '''
             #ヒストグラム算出用のフィルタテンソルを作成
             posBinCounter = VoteCount(binNum=self.__bin,
                                       detectorNum=detectorNum,
@@ -207,59 +198,45 @@ class CAdaBoost:
             negBinCounter = VoteCount(binNum=self.__bin,
                                       detectorNum=detectorNum,
                                       sampleNum=negSample)
+            '''
             remainDetIDList = np.arange(detectorNum)
             
             # スコアをBIN値に換算
             trainPosBin = (trainPosScore * self.__bin).astype(np.int64)
-            trainNegBin = (trainNegScore * self.__bin).astype(np.int64)   
+            trainNegBin = (trainNegScore * self.__bin).astype(np.int64)
             # 万が一値がBIN値と同じ場合はBIN-1としてカウントする
-            trainPosBin -= 1*(trainPosBin >= self.__bin)    
-            trainNegBin -= 1*(trainNegBin >= self.__bin)
+            trainPosBin = trainPosBin * (trainPosBin < self.__bin) + (self.__bin - 1) * (trainPosBin >= self.__bin)   
+            trainNegBin = trainNegBin * (trainNegBin < self.__bin) + (self.__bin - 1) * (trainNegBin >= self.__bin)
             # 負の値はとらないはずだが、一応確認
             assert(not (trainPosBin < 0).any())
             assert(not (trainNegBin < 0).any())
 
-            '''
-            histP = np.zeros(self.__bin,int)
-            histN = np.zeros(self.__bin,int)
-            for d in range(len(trainPosBin)):
-                for i in range(len(trainPosBin[d])):
-                    for b in range(self.__bin):
-                        if trainPosBin[d][i] == b:
-                            histP[b] += 1
-            for d in range(len(trainNegBin)):
-                for i in range(len(trainNegBin[d])):
-                    for b in range(self.__bin):
-                        if trainNegBin[d][i] == b:
-                            histN[b] += 1
-            plt.plot(np.arange(self.__bin)/self.__bin,histP/np.sum(histP),'.')
-            plt.plot(np.arange(self.__bin)/self.__bin,histN/np.sum(histN),'.')
-            plt.plot(np.arange(self.__bin)/self.__bin,(histP+histN)/np.sum(histP+histN),'.')
-            plt.show()
-            exit()
-            '''
-                            
             for w in range(detLen):
 
+                # まだAdaBoostに選択されず残っている識別器の数
+                detRemain = trainPosBin.shape[0]
+                
                 # 各識別器の性能を計算するための重み付きヒストグラム（識別器 x AdabootBin）を計算
-                histoPos = posBinCounter.calc(trainPosBin, posSampleWeight)
-                histoNeg = negBinCounter.calc(trainNegBin, negSampleWeight)
-
+                histoPos = np.zeros((detRemain, self.__bin))
+                histoNeg = np.zeros((detRemain, self.__bin))
+                for b in range(self.__bin):
+                    histoPos[np.arange(detRemain),b] = np.dot(trainPosBin == b, posSampleWeight)
+                    histoNeg[np.arange(detRemain),b] = np.dot(trainNegBin == b, negSampleWeight)
                 # 残っている弱識別器から最優秀のものを選択            
                 bestDet = np.argmin(np.sum(np.sqrt(histoPos * histoNeg), axis=1))
                 
                 #ゼロ割り回避
-                epsilon = 0.00001
+                epsilon = 1e-10
                 histoPos += epsilon
                 histoNeg += epsilon
                 
                 # 最優秀識別器の信頼性を算出
-                if self.__adaType == adaType.Real:
+                if self.__adaType == "Real":
                     h = np.log(histoPos[bestDet] + epsilon )/(histoNeg[bestDet] + epsilon)
-                elif self.__adaType == adaType.SaturatedReal:
+                elif self.__adaType == "SaturatedReal":
                     alpha = 0.4
-                    expPos = np.power(histoPos[bestDet], alpha)
-                    expNeg = np.power(histoNeg[bestDet], alpha)
+                    expPos = np.power(histoPos[bestDet], alpha) + self.__regularize
+                    expNeg = np.power(histoNeg[bestDet], alpha) + self.__regularize
                     h = ( expPos - expNeg) / (expPos + expNeg)
                 
                 strongDetBin[w] = h
@@ -286,12 +263,8 @@ class CAdaBoost:
                 trainPosBin = np.delete(trainPosBin, bestDet, axis=0)
                 trainNegBin = np.delete(trainNegBin, bestDet, axis=0)
                 remainDetIDList = np.delete(remainDetIDList, bestDet)
-                posSample -= 1
-                negSample -= 1
-                posBinCounter.reduceDetector()
-                negBinCounter.reduceDetector()
 
-                if (0 == (w + 1) % 100) or (w + 1 == detLen):
+                if (0 == (w + 1) % 1) or (w + 1 == detLen):
                     print("boosting weak detector:", w + 1)
             
                 assert(not np.any(np.isnan(strongDetBin)))
@@ -339,12 +312,17 @@ class CAdaBoost:
         self.__testScoreMat = np.transpose(self.__testScoreMat)
 
         finalScore = np.zeros(len(inImgList))
-        for d in range(strongDetectorID.size):
-            selectedDetectorID = strongDetectorID[d]
-            for i in range(len(inImgList)):
-                score = self.__testScoreMat[selectedDetectorID][i]
-                b = mt.IntMax(score * self.__bin, self.__bin - 1)
-                finalScore[i] += strongDetector[d][b]
+        if (self.__adaType == "Real") or (self.__adaType == "SaturatedReal"):
+            for d in range(strongDetectorID.size):
+                selectedDetectorID = strongDetectorID[d]
+                for i in range(len(inImgList)):
+                    score = self.__testScoreMat[selectedDetectorID][i]
+                    b = mt.IntMax(score * self.__bin, self.__bin - 1)
+                    finalScore[i] += strongDetector[d][b]
+        elif self.__adaType == "Discrete":
+            finalScore = np.dot(self.__detWeights, self.__decisionTree.predict(strongDetectorID, self.__testScoreMat)).T
+        else:
+            assert(0)
         outDict = {}
         outDict["finalScore"] = np.asarray(finalScore)
         outDict["label"] = np.asarray(inLabelList)
@@ -361,38 +339,35 @@ class CAdaBoost:
     
 if "__main__" == __name__:
 
-    TrainPosDir = "TrainPos"
-    TrainNegDir = "TrainNeg"
-    TestPosDir  = "TestPos"
-    TestNegDir  = "TestNeg"
-    detectorList = [CHog(CHogParam(bin=8, cellX=6,cellY=12,blockX=1,blockY=1))]
-    
-    trainImgList = []
-    trainLabelList = []
+    os.system("rm *mat")
+    lp = dirPath2NumpyArray("INRIAPerson/LearnPos")
+    ln = dirPath2NumpyArray("INRIAPerson/LearnNeg")
+    ep = dirPath2NumpyArray("INRIAPerson/EvalPos")
+    en = dirPath2NumpyArray("INRIAPerson/EvalNeg")
+    learn = RGB2Gray(lp + ln, "green")
+    eval  = RGB2Gray(ep + en, "green")
+    learnLabel = np.array([1] * len(lp) + [-1] * len(ln))
+    evalLabel  = np.array([1] * len(ep) + [-1] * len(en))
+    hogParam = CHogParam()
+    hogParam["Bin"] = 8
+    hogParam["Cell"]["X"] = 2
+    hogParam["Cell"]["Y"] = 4
+    hogParam["Block"]["X"] = 1
+    hogParam["Block"]["Y"] = 1
+    detectorList = [CHog(hogParam)]
 
-    for imgPath in fio.GetFileList(TrainPosDir):
-        trainImgList.append(imt.imgPath2ndarray(imgPath))
-        trainLabelList.append(1)
-    for imgPath in fio.GetFileList(TrainNegDir):
-        trainImgList.append(imt.imgPath2ndarray(imgPath))
-        trainLabelList.append(-1)
-    AdaBoost = CAdaBoost(inAdaType=adaType.SaturatedReal,
-                         inAdaBin=16,
-                         inImgList=trainImgList,
-                         inLabelList=trainLabelList,
-                         inDetectorList=detectorList,
-                         loopNum=4000)
-    
-    testImgList = []
-    testLabelList = []
+    adaBoostParam = AdaBoostParam
+    AdaBoostParam["Bin"] = 10000
+    AdaBoostParam["Regularizer"] = 1e-5
+    AdaBoostParam["Bin"] = 32
 
-    for imgPath in fio.GetFileList(TestPosDir):
-        testImgList.append(imt.imgPath2ndarray(imgPath))
-        testLabelList.append(1)
-    for imgPath in fio.GetFileList(TestNegDir):
-        testImgList.append(imt.imgPath2ndarray(imgPath))
-        testLabelList.append(-1)
-    AdaBoost.Evaluate(inImgList=testImgList,inLabelList=testLabelList)
+    AdaBoost = CAdaBoost(inAdaBoostParam=adaBoostParam,
+                         inImgList=learn,
+                         inLabelList=learnLabel,
+                         inDetectorList=detectorList)
     
-    gui.DrawROC(np.asarray(sio.loadmat("FinalScore.mat")["finalScore"])[0],
+    AdaBoost.Evaluate(inImgList=eval,inLabelList=evalLabel)
+    
+    accuracy, auc = gui.evaluateROC(np.asarray(sio.loadmat("FinalScore.mat")["finalScore"])[0],
         np.asarray(sio.loadmat("FinalScore.mat")["label"])[0])
+    print(auc)
