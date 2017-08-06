66d78233b0da6b59d1234673adc9db0a442a4033
Yusuke Goto <me@example.com>
2017-08-06 15:12:20 +0900

    画像ファイル入力用のnumpy変換モジュールを追加

---
+++ input.py
@@ -0,0 +1,20 @@
+from PIL import Image
+import numpy as np
+from common.origLib import *
+from common.fileIO import *
+from common.imgtool import *
+
+def dirPath2NumpyArray(dirPath):
+
+    out = []
+    
+    for imgPath in GetFileList(dirPath):
+        pilImg = Image.open(imgPath)
+        npImg = np.asarray(pilImg)
+        
+        if npImg.ndim >= 3:
+            if npImg.shape[2] >= 4:
+                npImg = npImg[:,:,:3]
+        out.append(npImg)
+        
+    return np.array(out)
