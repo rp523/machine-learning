e1c5533ae5ba15b9dc3fc3c5dd0bec828ea96adc
Yusuke Goto <me@example.com>
2017-08-17 12:00:34 +0900

    t-SNEでのassertを全て有効に戻す(大して速度は変わらない)

--- t-SNE.py
+++ t-SNE.py
@@ -21,7 +21,7 @@ class CtSNE:
 
     def __call__(self, x):
         #assert(isinstance(x, np.ndarray))
-        assert(x.ndim == 2)
+        #assert(x.ndim == 2)
         sampleNum, featureLen = x.shape
         
         # 正規分布で初期化
@@ -45,21 +45,21 @@ class CtSNE:
             crossDiffY = np.empty((sampleNum, sampleNum, self.__mapDim))
             crossDiffY[np.arange(sampleNum),:,:] = y    # (N x N x dim)
             crossDiffY = crossDiffY.transpose(1, 0, 2) - crossDiffY
-            #assert(crossDiffY.shape == (sampleNum, sampleNum, self.__mapDim))
+            assert(crossDiffY.shape == (sampleNum, sampleNum, self.__mapDim))
             crossDiffY = crossDiffY.transpose(2, 0, 1)  # (dim x N x N)
-            #assert(crossDiffY.shape == (self.__mapDim, sampleNum, sampleNum))
+            assert(crossDiffY.shape == (self.__mapDim, sampleNum, sampleNum))
             
             gradYMat = (p - q) * crossDiffY / (1.0 + np.sum(crossDiffY ** 2, axis = 0)) # (dim x N x N)
             gradY = 4 * np.sum(gradYMat, axis = 1).T    # (N x dim)
-            #assert(gradY.shape == (sampleNum, self.__mapDim))
+            assert(gradY.shape == (sampleNum, self.__mapDim))
             
             yOld = y.copy()
             
             y = y + self.__eta * gradY + self.__alpha(l) * (y - before1)
             before1 = yOld
             
-            if (l % 10 == 0) or (l == self.__loop - 1):
-                #assert(p.shape == q.shape)
+            if (l % 10 == 9) or (l == self.__loop - 1):
+                assert(p.shape == q.shape)
                 noDiagIdx = np.bitwise_not(np.eye(q.shape[0]).astype(np.bool))
                 kl_div = np.sum(p[noDiagIdx] * np.log(p[noDiagIdx] / q[noDiagIdx]))
                 print("loop=", l + 1, "/", self.__loop, "kl-div=", kl_div)
@@ -68,21 +68,21 @@ class CtSNE:
     
     def __Perp2SqSigma(self, sqDist, perpTgt):
         
-        #assert(isinstance(sqDist, np.ndarray))
-        #assert(sqDist.ndim == 2)
-        #assert(sqDist.shape[0] == sqDist.shape[1])
+        assert(isinstance(sqDist, np.ndarray))
+        assert(sqDist.ndim == 2)
+        assert(sqDist.shape[0] == sqDist.shape[1])
         sampleNum = sqDist.shape[0]
         
         def perpTry(sqDist, sqSigma):
             sampleNum = sqDist.shape[0]
             p = self.__CalcHighDimProb(sqDist, sqSigma)
-            #assert(not np.isnan(p).any())
-            #assert(p.shape == (sampleNum, sampleNum))
+            assert(not np.isnan(p).any())
+            assert(p.shape == (sampleNum, sampleNum))
             
             #debug
             p[np.eye(sampleNum).astype(np.bool)] = 1
             H = np.sum( - p * np.log2(p), axis = 1)
-            #assert(not np.isnan(H).any())
+            assert(not np.isnan(H).any())
             out = 2 ** H
             return out
 
@@ -92,7 +92,7 @@ class CtSNE:
         while((perpTry(sqDist, sqSigmaMin) >= perpTgt).any()):
             sqSigmaMin *= 0.5
             print("binary search min-setting:",cnt + 1);cnt += 1
-        #assert((perpTry(sqDist, sqSigmaMin) < perpTgt).all())
+        assert((perpTry(sqDist, sqSigmaMin) < perpTgt).all())
 
         # initialize larger end of search window
         sqSigmaMax = np.ones(sampleNum)
@@ -100,7 +100,7 @@ class CtSNE:
         while((perpTry(sqDist, sqSigmaMax) <= perpTgt).any()):
             sqSigmaMax *= 2.0
             print("binary search max-setting:",cnt + 1);cnt += 1
-        #assert((perpTry(sqDist, sqSigmaMax) > perpTgt).all())
+        assert((perpTry(sqDist, sqSigmaMax) > perpTgt).all())
 
         # initialize center of search window
         sqSigmaCenter = 0.5 * (sqSigmaMin + sqSigmaMax)
@@ -115,28 +115,28 @@ class CtSNE:
 
         breakWhile = False
         for l in range(100000000):
-            #assert((perpTry(sqDist, sqSigmaMax) > perpTgt).all())
-            #assert((perpTry(sqDist, sqSigmaMax)[np.bitwise_not(OK)] >= perpTgt).all())
+            assert((perpTry(sqDist, sqSigmaMax) > perpTgt).all())
+            assert((perpTry(sqDist, sqSigmaMax)[np.bitwise_not(OK)] >= perpTgt).all())
             
             perpTryVal = perpTry(sqDist, sqSigmaCenter) # full length
             
             isTooLarge = (perpTryVal > perpTgt)
             if isTooLarge.any():
-                #assert(sqSigmaMax[isTooLarge].size == np.sum(isTooLarge))
+                assert(sqSigmaMax[isTooLarge].size == np.sum(isTooLarge))
                 sqSigmaMax[isTooLarge] = sqSigmaCenter[isTooLarge]
             
             isTooSmall = (perpTryVal < perpTgt)
             if isTooSmall.any():
-                #assert(sqSigmaMin[isTooSmall].size == np.sum(isTooSmall))
+                assert(sqSigmaMin[isTooSmall].size == np.sum(isTooSmall))
                 sqSigmaMin[isTooSmall] = sqSigmaCenter[isTooSmall]
 
             sqSigmaCenter = 0.5 * (sqSigmaMin + sqSigmaMax)
-            #assert((sqSigmaMin < sqSigmaCenter).all())
-            #assert((sqSigmaMax > sqSigmaCenter).all())
-            #assert((0.0 < sqSigmaMin).all())
-            #assert((0.0 < sqSigmaMax).all())
-            #assert((0.0 < sqSigmaCenter).all())
-            #assert((0.0 < outSqSigma).all())
+            assert((sqSigmaMin < sqSigmaCenter).all())
+            assert((sqSigmaMax > sqSigmaCenter).all())
+            assert((0.0 < sqSigmaMin).all())
+            assert((0.0 < sqSigmaMax).all())
+            assert((0.0 < sqSigmaCenter).all())
+            assert((0.0 < outSqSigma).all())
             
             if not OK.all():
                 # まだ終わってない
@@ -162,8 +162,8 @@ class CtSNE:
     
     # calculate squared feature distance in high-dimensional (original) space
     def __CalcHighDimSquareDistance(self, x):
-        #assert(isinstance(x, np.ndarray))
-        #assert(x.ndim == 2)
+        assert(isinstance(x, np.ndarray))
+        assert(x.ndim == 2)
         sampleNum, featureLen = x.shape
         
         # 特徴量ペアのユークリッド距離の２乗行列を作る。
@@ -177,8 +177,8 @@ class CtSNE:
 
     # calculate squared feature distance in low-dimensional (mapped) space
     def __CalcLowDimSquareDistance(self, y):
-        #assert(isinstance(y, np.ndarray))
-        #assert(y.ndim == 2)
+        assert(isinstance(y, np.ndarray))
+        assert(y.ndim == 2)
         sampleNum, featureLen = y.shape
         
         diff2 = np.empty((sampleNum, sampleNum, featureLen)).astype(np.float)
@@ -189,15 +189,13 @@ class CtSNE:
     
     # calculate conditional probability distribution in high-dimensional (original) space
     def __CalcHighDimProb(self, sqDist, sqSigma):
-        #assert(isinstance(sqDist, np.ndarray))
-        #assert(sqDist.ndim == 2)
-        #assert(sqDist.shape[0] == sqDist.shape[1])
-        #assert(isinstance(sqSigma, np.ndarray))
-        #assert(sqSigma.ndim == 1)
-        if not ((sqSigma > 0.0).all()):
-            print(sqSigma)
-        #assert((sqSigma > 0.0).all())
-        #assert((sqDist == sqDist.T).all())
+        assert(isinstance(sqDist, np.ndarray))
+        assert(sqDist.ndim == 2)
+        assert(sqDist.shape[0] == sqDist.shape[1])
+        assert(isinstance(sqSigma, np.ndarray))
+        assert(sqSigma.ndim == 1)
+        assert((sqSigma > 0.0).all())
+        assert((sqDist == sqDist.T).all())
         
         sampleNum = sqDist.shape[0]
 
@@ -205,29 +203,29 @@ class CtSNE:
 
         power = power - np.max(power, axis = 1)   # オーバーフロー防止
         neighbor = np.exp(power)
-        #assert((neighbor > 0.0).all())
+        assert((neighbor > 0.0).all())
 
         # 対角成分をゼロとする
         neighbor[np.eye(sampleNum).astype(np.bool)] = 0.0
-        #assert(neighbor.shape == (sampleNum, sampleNum))
+        assert(neighbor.shape == (sampleNum, sampleNum))
         bunbo = np.sum(neighbor, axis = 1)
-        #assert(bunbo.shape == (sampleNum,))
-        #assert((bunbo > 0.0).all())
+        assert(bunbo.shape == (sampleNum,))
+        assert((bunbo > 0.0).all())
 
         prob = neighbor / bunbo.reshape(-1, 1)
         
-        #assert(prob.shape == (sampleNum, sampleNum))
-        #assert((0.0 < prob[np.bitwise_not(np.eye(sampleNum).astype(bool))]).all())        
+        assert(prob.shape == (sampleNum, sampleNum))
+        assert((0.0 < prob[np.bitwise_not(np.eye(sampleNum).astype(bool))]).all())        
 
         return prob
 
     # calculate conditional probability distribution in low-dimensional (mapped) space
     def __CalcLowDimProb(self, sqDist):
-        #assert(isinstance(sqDist, np.ndarray))
-        #assert(sqDist.ndim == 2)
-        #assert(sqDist.shape[0] == sqDist.shape[1])
-        #assert((sqDist[np.bitwise_not(np.eye(sqDist.shape[0]).astype(np.bool))] > 0.0).all())
-        #assert(not np.isnan(sqDist).any())
+        assert(isinstance(sqDist, np.ndarray))
+        assert(sqDist.ndim == 2)
+        assert(sqDist.shape[0] == sqDist.shape[1])
+        assert((sqDist[np.bitwise_not(np.eye(sqDist.shape[0]).astype(np.bool))] > 0.0).all())
+        assert(not np.isnan(sqDist).any())
         
         sampleNum = sqDist.shape[0]
 
@@ -235,14 +233,14 @@ class CtSNE:
 
         # 対角成分をゼロとする
         neighbor[np.eye(sampleNum).astype(np.bool)] = 0.0
-        #assert(neighbor.shape == (sampleNum, sampleNum))
+        assert(neighbor.shape == (sampleNum, sampleNum))
         
         bunbo = np.sum(neighbor) * 0.5  # i!=jを全て足しあげる
-        #assert(bunbo > 0.0)
+        assert(bunbo > 0.0)
         
         prob = neighbor / bunbo
-        #assert((prob[np.bitwise_not(np.eye(prob.shape[0]).astype(np.bool))] > 0.0).all())
-        #assert((prob <= 1.0).all())        
+        assert((prob[np.bitwise_not(np.eye(prob.shape[0]).astype(np.bool))] > 0.0).all())
+        assert((prob <= 1.0).all())        
         
         return prob
 
@@ -250,24 +248,26 @@ if "__main__" == __name__:
     
     # prepare class for t-SNE
     tSNE = CtSNE(mapDim = 2,
-                 perplexity=5,
-                 loop=1000)
+                 perplexity = 10,
+                 loop = 1000)
     
     # prepare toy datasets
-    dat = np.empty((0, 28, 28))
+    dat = np.empty(0)
     label = np.empty(0)
     print("Reading Start.")
     for i in range(10):
         print("Reading: ", str(i))
         eval = dirPath2NumpyArray("dataset/mnist/eval/" + str(i)) / 255
-        #index = np.arange(eval.shape[0])#np.random.choice(np.arange(eval.shape[0]), 600, replace=False)
+        #index = np.arange(eval.shape[0])
         index = np.random.choice(np.arange(eval.shape[0]), 50, replace=False)
         eval = eval[index]
-        dat = np.append(dat, eval, axis = 0)
+        if 0 == dat.size:
+            dat = np.empty((0, eval.size // eval.shape[0]))
+        dat = np.append(dat, eval.reshape(eval.shape[0], -1), axis = 0)
         label = np.append(label, np.array([i] * eval.shape[0])).astype(np.int)
     print("Finished reading.")
-    x = dat.reshape(-1, 28 * 28)
-    y = tSNE(x)
+    #x = dat.reshape(dat.shape[0], -1)
+    y = tSNE(dat)
 
     # draw results
     plt.figure()
